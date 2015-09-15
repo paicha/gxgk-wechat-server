9198d2217fa9f338c64a882683275430f076e64c
赖嘉伟 <paicha.me@gmail.com>
2015-09-16 02:19:19 +0800

    保存最后一次交互时间

--- main/plugins/state.py
+++ main/plugins/state.py
@@ -13,3 +13,14 @@ def set_user_state(openid, state):
 def get_user_state(openid):
     """获取用户状态"""
     return redis.hget('wechat:user:' + openid, 'state')
+
+
+def set_user_last_interact_time(openid, timestamp):
+    """保存最后一次交互时间"""
+    redis.hset('wechat:user:' + openid, 'last_interact_time', timestamp)
+    return None
+
+
+def get_user_last_interact_time(openid):
+    """获取最后一次交互时间"""
+    return redis.hget('wechat:user:' + openid, 'last_interact_time')
