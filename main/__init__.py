a5af95e44362d880d60fdf1f9c0ee5c2a0681113
赖嘉伟 <paicha.me@gmail.com>
2015-10-01 16:23:43 +0800

    日志输出

--- main/__init__.py
+++ main/__init__.py
@@ -2,13 +2,26 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+import logging
+from logging.handlers import RotatingFileHandler
 from redis import Redis
 from wechat_sdk import WechatBasic
 
 app = Flask(__name__, instance_relative_config=True)
+# 加载配置
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
+# 记录日志
+handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s '
+    '[in %(pathname)s:%(lineno)d]'
+))
+handler.setLevel(logging.WARNING)
+app.logger.addHandler(handler)
+
+# 初始第三方库
 redis = Redis()
 wechat = WechatBasic(appid=app.config['APP_ID'],
                      appsecret=app.config['APP_SECRET'],
