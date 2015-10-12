00f668ab71f86aa23f8278fe9d8f522d914d3aa7
赖嘉伟 <paicha.me@gmail.com>
2015-10-12 19:38:19 +0800

    增加音乐功能

---
+++ main/plugins/music.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+from .. import app
+from .. import celery
+import wechat_custom
+
+
+@celery.task
+def get_douban_fm(openid):
+    """抓取豆瓣FM"""
+    url = 'http://www.douban.com/j/app/radio/people?' + \
+        'app_name=radio_desktop_win&version=100&channel=0&type=n'
+    try:
+        r = requests.get(url, timeout=5)
+        result = r.json()["song"][0]
+        desc = result["artist"] + u'-建议WiFi下播放'
+        music_url = result["url"]
+        title = result["title"]
+    except Exception, e:
+        app.logger.warning(u"豆瓣FM请求或解析失败: %s" % e)
+        context = u"网络繁忙，请稍候重试"
+        wechat_custom.send_text(openid, context)
+    else:
+        # 客服接口推送音乐必须要有 thumb_media_id
+        thumb_media_id = app.config["THUMB_MEDIA_ID"]
+        wechat_custom.send_music(openid, title, desc, music_url, thumb_media_id)
