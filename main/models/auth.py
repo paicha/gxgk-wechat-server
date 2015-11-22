bdc0bad4149b92eed556cae24ec29c26c63028e4
赖嘉伟 <paicha.me@gmail.com>
2015-11-22 17:27:40 +0800

    修正数据库字段名

--- main/models/auth.py
+++ main/models/auth.py
@@ -13,17 +13,17 @@ class Auth(db.Model):
     openid = db.Column(db.String(32), primary_key=True, unique=True,
                        nullable=False)
     studentid = db.Column(db.String(20), nullable=True)
-    studenidpwd = db.Column(db.String(40), nullable=True)
+    studentpwd = db.Column(db.String(40), nullable=True)
     libraryid = db.Column(db.String(20), nullable=True)
-    libraryidpwd = db.Column(db.String(40), nullable=True)
+    librarypwd = db.Column(db.String(40), nullable=True)
 
-    def __init__(self, openid, studentid, studenidpwd, libraryid,
-                 libraryidpwd):
+    def __init__(self, openid, studentid, studentpwd, libraryid,
+                 librarypwd):
         self.openid = openid
         self.studentid = studentid
-        self.studenidpwd = studenidpwd
+        self.studentpwd = studentpwd
         self.libraryid = libraryid
-        self.libraryidpwd = libraryidpwd
+        self.librarypwd = librarypwd
 
     def __repr__(self):
         return '<openid %r>' % self.openid
