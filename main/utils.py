6b92dc32a2226a3cc7c7bbea73a4d44d0c58df4a
赖嘉伟 <paicha.me@gmail.com>
2015-11-27 23:29:55 +0800

    增加前端微信 jssdk 配置生成方法

--- main/utils.py
+++ main/utils.py
@@ -4,6 +4,9 @@
 from functools import wraps
 from flask import request, redirect
 from . import app, wechat, redis
+import time
+import random
+import string
 
 
 def check_signature(func):
@@ -37,3 +40,23 @@ def get_wechat_access_token():
     else:
         app.logger.warning(u"获取 access_token 缓存失败")
         return None
+
+
+def get_jsapi_signature_data(url):
+    """
+    获取 jsapi 前端签名数据
+    """
+    timestamp = int(time.time())
+    noncestr = generate_random_str(16)
+    signature = wechat.generate_jsapi_signature(timestamp, noncestr, url)
+    return {
+        "appId": app.config['APP_ID'],
+        "timestamp": timestamp,
+        "nonceStr": noncestr,
+        "signature": signature,
+    }
+
+
+def generate_random_str(N):
+    """生成随机字符串"""
+    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
