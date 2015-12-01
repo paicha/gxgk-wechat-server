d7f3df52b346ebc30c34e9862fad52d4a2a0b437
赖嘉伟 <paicha.me@gmail.com>
2015-12-01 16:45:15 +0800

    更正数据写入默认参数

--- main/models/auth.py
+++ main/models/auth.py
@@ -17,8 +17,8 @@ class Auth(db.Model):
     libraryid = db.Column(db.String(20), nullable=True)
     librarypwd = db.Column(db.String(100), nullable=True)
 
-    def __init__(self, openid, studentid, studentpwd, libraryid,
-                 librarypwd):
+    def __init__(self, openid, studentid=None, studentpwd=None, libraryid=None,
+                 librarypwd=None):
         self.openid = openid
         self.studentid = studentid
         self.studentpwd = studentpwd
