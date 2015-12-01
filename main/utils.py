bf7ee2030cd3bb9dd45c9eacc4b6815e1cd9297e
赖嘉伟 <paicha.me@gmail.com>
2015-12-01 17:05:24 +0800

    密码加密解密方法

--- main/utils.py
+++ main/utils.py
@@ -4,9 +4,12 @@
 from functools import wraps
 from flask import request, redirect
 from . import app, wechat, redis
+from Crypto.Cipher import AES
+from Crypto import Random
 import time
 import random
 import string
+import base64
 
 
 def check_signature(func):
@@ -60,3 +63,30 @@ def get_jsapi_signature_data(url):
 def generate_random_str(N):
     """生成随机字符串"""
     return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
+
+
+class AESCipher:
+
+    """
+    加密解密方法
+    http://stackoverflow.com/questions/12524994
+    """
+
+    def __init__(self, key):
+        self.BS = 16
+        self.pad = lambda s: s + \
+            (self.BS - len(s) % self.BS) * chr(self.BS - len(s) % self.BS)
+        self.unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+        self.key = key
+
+    def encrypt(self, raw):
+        raw = self.pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self.unpad(cipher.decrypt(enc[16:]))
