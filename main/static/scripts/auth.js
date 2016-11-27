b3ca3d062b1b926805a3aa1a35238f4f5d7be5aa
qichhhhh <464105626@qq.com>
2016-11-27 10:24:46 +0800

    [*]zepto.js v1.1.6 → v1.2.0 [*]zepto[event ajax form ie] -> zepto[event ajax touch fx_methods] [*]weui v0.4.x → weui v1.1.0

--- main/static/scripts/auth.js
+++ main/static/scripts/auth.js
@@ -2,30 +2,37 @@ $(function() {
 
     function get_auth_result() {
         var intervalId = setInterval(function() {
-            $.get(window.location.href + '/result', function(res) {
+            $.get(window.location.href + '/result',
+            function(res) {
                 clearInterval(intervalId);
-                $('#loadingToast').hide();
+                $('#loadingToast').fadeOut(100);
                 if (res.errmsg === 'ok') {
                     $('.page.msg').show();
                     // 绑定成功3秒后关闭窗口
                     setTimeout(function() {
                         wx.closeWindow();
-                    }, 3000);
+                    },
+                    3000);
                 } else {
                     // 绑定失败，显示后端信息
                     $('#err_msg').text(res.errmsg);
-                    $('.weui_dialog_alert').show();
+                    $('#iosDialog1').fadeIn(200);
                 }
             });
-        }, 1000);
+        },
+        1000);
     }
 
     $('#submit').tap(function() {
+        // 如果正在查询中，则不发起请求
+        if ($('#loadingToast').css('display') != 'none') return;
+        // 如果正在显示错误信息，则不发起请求
+        if ($('.js_tooltips').css('display') != 'none') return;
         var username = $('#username').val().replace(/\s+/g, '');
         var password = $('#password').val().replace(/\s+/g, '');
         // 验证各项信息不为空
-        if (!!username && !!password) {
-            $('#loadingToast').show();
+        if ( !! username && !!password) {
+            $('#loadingToast').fadeIn(100);
             // 判断绑定类型
             var data;
             if ($('.page_title').text() === '微信查成绩') {
@@ -40,21 +47,23 @@ $(function() {
                 };
             }
             // 提交绑定信息
-            $.post(window.location.href, data, function(res) {
+            $.post(window.location.href, data,
+            function(res) {
                 if (res.errmsg === 'ok') {
                     get_auth_result();
                 }
             });
         } else {
             // 提示输入格式不正确
-            $('.js_tooltips').show();
+            $('.js_tooltips').css('display', 'block');
             setTimeout(function() {
-                $('.js_tooltips').hide();
-            }, 3000);
+                $('.js_tooltips').css('display', 'none');
+            },
+            3000);
         }
     });
     // 关闭错误弹框
-    $('.weui_btn_dialog.primary').tap(function() {
-        $('.weui_dialog_alert').hide();
+    $('.weui-dialog__btn.weui-dialog__btn_primary').tap(function() {
+        $('#iosDialog1').fadeOut(200);
     });
 });
